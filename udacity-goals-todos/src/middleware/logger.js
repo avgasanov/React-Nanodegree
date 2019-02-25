65d2ad45409d72764ca03d97d62c542070f8f195
AVGasanov <avgasanov@gmail.com>
2019-02-26 01:55:13 +0400

    Add final solution for "Part 3. Module 1. Lesson 6"
    - Update Course lecture

---
+++ udacity-goals-todos/src/middleware/logger.js
@@ -0,0 +1,10 @@
+const logger = (store) => (next) => (action) => {
+    console.group(action.type)
+        console.log('The action: ', action)
+        console.log('The old state: ', store.getState())
+        const result = next(action)
+        console.log('The new state: ', store.getState())
+    console.groupEnd()
+}
+
+export default logger
