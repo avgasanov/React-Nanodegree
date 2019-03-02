682731cfca66fd4b3a64f1912ad390819ccd9c0c
AVGasanov <avgasanov@gmail.com>
2019-03-02 19:42:10 +0400

    Add solution for "Part 4. Module 1. Lesson 2"
    - Update Course lecture

--- UdaciFitness/App.js
+++ UdaciFitness/App.js
@@ -1,28 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/addEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log('Before')
-    debugger
-    console.log('After')
-
-  }
-
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View>
+            <AddEntry/>
+        </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
